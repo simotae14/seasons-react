ed1741f2d8b0078a871244874c5c281ebf47bef6
Simone <startae14@gmail.com>
2021-02-18 11:25:09 +0100

    05. use setState to change the latitude inside the component

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
     this.state = {
       lat: null
     }
-  }
-  render() {
+
     // try the geolocation
     window.navigator.geolocation.getCurrentPosition(
       // callback on success
-      (position) => console.log(position),
+      position => {
+        this.setState({
+          lat: position?.coords?.latitude
+        });
+      },
       // callback on error
-      (err) => console.log(err)
+      err => console.log(err)
     );
+  }
+  render() {
     return (
-      <div>Latitude: </div>
+      <div>Latitude: {this.state.lat}</div>
     );
   }
 }
