5e183e2924b1d61b46195752cd58ec1d96c214bb
Simone <startae14@gmail.com>
2021-02-18 01:29:17 +0100

    02. getting a user Geolocation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (<div>Hi there</div>);
+const App = () => {
+  // try the geolocation
+  window.navigator.geolocation.getCurrentPosition(
+    // callback on success
+    (position) => console.log(position),
+    // callback on error
+    (err) => console.log(err)
+  );
+  return (<div>Hi there</div>)
+};
 
 ReactDOM.render(
   <App />,
