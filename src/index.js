6ecac28d157b6880ff33086fcfd699d1e6b756c9
Simone <startae14@gmail.com>
2021-02-18 01:49:47 +0100

    03. use a Class Component for the App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  // try the geolocation
-  window.navigator.geolocation.getCurrentPosition(
-    // callback on success
-    (position) => console.log(position),
-    // callback on error
-    (err) => console.log(err)
-  );
-  return (<div>Hi there</div>)
-};
+class App extends React.Component {
+  render() {
+    // try the geolocation
+    window.navigator.geolocation.getCurrentPosition(
+      // callback on success
+      (position) => console.log(position),
+      // callback on error
+      (err) => console.log(err)
+    );
+    return (
+      <div>Latitude: </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
