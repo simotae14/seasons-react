6c591a72bd8dbf79f01c87527a20277df24776f9
Simone <startae14@gmail.com>
2021-02-21 18:56:27 +0100

    09. alternative state initialization

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // THIS IS THE ONLY TIME we do direct assignment
-    // to this.state
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    }
+  state = {
+    lat: null,
+    errorMessage: ''
   }
 
   componentDidMount() {
