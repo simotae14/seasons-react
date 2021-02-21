4d1a0cb50a8a5fdc48a06ddd731ce41f74b8cdf7
Simone <startae14@gmail.com>
2021-02-21 20:15:52 +0100

    14. create a configuration object to retrieve the proper data for the actual season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -10,14 +21,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth());
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];
+  console.log(iconName);
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>
-        { season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach' }
+        {text}
       </h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
