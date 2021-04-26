61e68074a18929209eb745825138486cc248f48e
Simone <startae14@gmail.com>
2021-04-27 00:56:40 +0200

    18. use only one return in the render function and manage the different conditions inside an helper function, like renderContent

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  // helper function to render the content
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -29,6 +30,14 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
